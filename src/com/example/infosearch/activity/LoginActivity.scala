788d4d5d4dff2cbb3299958743ababfc14ac0a57
firefoxmmx2 <firefoxmmx2>
2013-08-20 23:07:35 +0800

    登录页面

---
+++ src/com/example/infosearch/activity/LoginActivity.scala
@@ -0,0 +1,26 @@
+package com.example.infosearch.activity
+
+import com.example.infosearch.common.Widgets._
+import android.os.Bundle
+import android.widget._
+import android.content.Intent
+import com.example.infosearch.R
+
+class LoginActivity extends RichActivity {
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val loginBtn = get[Button](R.id.login_loginBtn)
+    val registerBtn = get[Button](R.id.login_registerBtn)
+    val username = get[EditText](R.id.login_username)
+    val password = get[EditText](R.id.login_password)
+
+    loginBtn.onClick({
+      val intent = new Intent(LoginActivity.this, classOf[MenuActivity])
+      startActivity(intent)
+      finish()
+    })
+  }
+
+}
