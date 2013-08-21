19f3b8eac7c8e06a3cc8347abb204a3f8f0caaac
firefoxmmx2 <firefoxmmx@gmail.com>
2013-08-21 16:53:18 +0800

    添加注册

--- src/com/example/infosearch/activity/LoginActivity.scala
+++ src/com/example/infosearch/activity/LoginActivity.scala
@@ -7,20 +7,27 @@ import android.content.Intent
 import com.example.infosearch.R
 
 class LoginActivity extends RichActivity {
-  override def onCreate(savedInstanceState: Bundle): Unit = {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+	override def onCreate(savedInstanceState: Bundle): Unit = {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-    val loginBtn = get[Button](R.id.login_loginBtn)
-    val registerBtn = get[Button](R.id.login_registerBtn)
-    val username = get[EditText](R.id.login_username)
-    val password = get[EditText](R.id.login_password)
+		val loginBtn = get[Button](R.id.login_loginBtn)
+		val registerBtn = get[Button](R.id.login_registerBtn)
+		val username = get[EditText](R.id.login_username)
+		val password = get[EditText](R.id.login_password)
 
-    loginBtn.onClick({
-      val intent = new Intent(LoginActivity.this, classOf[MenuActivity])
-      startActivity(intent)
-      finish()
-    })
-  }
+		loginBtn.onClick({
+			val intent = new Intent(LoginActivity.this, classOf[MenuActivity])
+			startActivity(intent)
+			finish()
+		})
+
+		registerBtn.onClick({
+			val intent = new Intent(LoginActivity.this, classOf[RegisterActivity])
+			startActivity(intent)
+
+		})
+
+	}
 
 }
---
+++ src/com/example/infosearch/activity/RegisterActivity.scala
@@ -0,0 +1,12 @@
+package com.example.infosearch.activity
+
+import android.app.Activity
+import com.example.infosearch.common.Widgets._
+import android.os.Bundle
+
+class RegisterActivity extends Activity with RichActivity {
+	override def onCreate(savedInstanceState: Bundle): Unit = {
+		super.onCreate(savedInstanceState)
+
+	}
+}
