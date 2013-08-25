db1d17882f5be102bf9bed6c1bc8d9cc11826dd7
firefoxmmx2 <firefoxmmx2>
2013-08-25 23:22:38 +0800

    添加用户注册服务

--- src/com/example/infosearch/business/User.scala
+++ src/com/example/infosearch/business/User.scala
@@ -9,65 +9,74 @@ import java.text.SimpleDateFormat
 import java.text.ParseException
 
 object UserService {
-	def register(context: Context, username: EditText,
-		password: EditText,
-		passwordRepeat: EditText,
-		email: EditText,
-		birth: EditText,
-		mobile: EditText): Unit = {
+  val sdf = new SimpleDateFormat("yyyy-MM-dd")
 
-	}
-	def add(user: User) = {
+  def register(context: Context, username: EditText,
+               password: EditText,
+               passwordRepeat: EditText,
+               email: EditText,
+               birth: EditText,
+               mobile: EditText): Unit = {
+    if (regValidate(context, username, password, passwordRepeat, email, birth, mobile)) {
 
-	}
+      add(User(username = username.getText().toString(),
+        password = password.getText().toString(),
+        email = email.getText().toString(),
+        birth = sdf.parse(birth.getText().toString()),
+        mobile = Option(mobile.getText().toString())))
+    }
+  }
+  def add(user: User) = {
 
-	def update(user: User) = {
+  }
 
-	}
+  def update(user: User) = {
 
-	def delete(user: User) = {
+  }
 
-	}
+  def delete(user: User) = {
 
-	def regValidate(context: Context, username: EditText,
-		password: EditText,
-		passwordRepeat: EditText,
-		email: EditText,
-		birth: EditText,
-		mobile: EditText): Boolean = {
-		if (username.getText().toString().length() < 5) {
-			Toast.makeText(context, "用户名必须大于等于5个字符", Toast.LENGTH_LONG).show()
-			return false
-		}
-		if (password.getText().toString().length() < 5) {
-			Toast.makeText(context, "密码必须大于等于5个字符", Toast.LENGTH_LONG).show()
-			return false
-		}
-		if (passwordRepeat.getText().toString().length() < 5) {
-			Toast.makeText(context, "重复密码必须大于等于5个字符", Toast.LENGTH_LONG).show()
-			return false
-		}
-		if (password.getText().toString() != passwordRepeat.getText().toString()) {
-			Toast.makeText(context, "两次输入的密码必须相同", Toast.LENGTH_LONG).show()
-			return false
-		}
-		if (!"\\d*".r.findAllIn(mobile.getText().toString()).hasNext) {
-			Toast.makeText(context, "手机号码需要是数字", Toast.LENGTH_LONG).show()
-			return false
-		}
-		if (email.getText().toString().length() == 0) {
-			Toast.makeText(context, "邮箱必须输入", Toast.LENGTH_LONG).show()
-			return false
-		}
-		try {
-			val sdf = new SimpleDateFormat("yyyy-MM-dd")
-			sdf.parse(birth.getText().toString())
-		} catch {
-			case e: ParseException => {
-				Toast.makeText(context, "不符合日期格式", Toast.LENGTH_LONG).show()
-				return false
-			}
-		}
-		true
-	}
+  }
+
+  def regValidate(context: Context, username: EditText,
+                  password: EditText,
+                  passwordRepeat: EditText,
+                  email: EditText,
+                  birth: EditText,
+                  mobile: EditText): Boolean = {
+    if (username.getText().toString().length() < 5) {
+      Toast.makeText(context, "用户名必须大于等于5个字符", Toast.LENGTH_LONG).show()
+      return false
+    }
+    if (password.getText().toString().length() < 5) {
+      Toast.makeText(context, "密码必须大于等于5个字符", Toast.LENGTH_LONG).show()
+      return false
+    }
+    if (passwordRepeat.getText().toString().length() < 5) {
+      Toast.makeText(context, "重复密码必须大于等于5个字符", Toast.LENGTH_LONG).show()
+      return false
+    }
+    if (password.getText().toString() != passwordRepeat.getText().toString()) {
+      Toast.makeText(context, "两次输入的密码必须相同", Toast.LENGTH_LONG).show()
+      return false
+    }
+    if (!"\\d*".r.findAllIn(mobile.getText().toString()).hasNext) {
+      Toast.makeText(context, "手机号码需要是数字", Toast.LENGTH_LONG).show()
+      return false
+    }
+    if (email.getText().toString().length() == 0) {
+      Toast.makeText(context, "邮箱必须输入", Toast.LENGTH_LONG).show()
+      return false
+    }
+    try {
+
+      sdf.parse(birth.getText().toString())
+    } catch {
+      case e: ParseException => {
+        Toast.makeText(context, "不符合日期格式", Toast.LENGTH_LONG).show()
+        return false
+      }
+    }
+    true
+  }
 }
--- src/com/example/infosearch/domain/User.scala
+++ src/com/example/infosearch/domain/User.scala
@@ -3,10 +3,10 @@ package com.example.infosearch.domain
 import java.util.Date
 
 case class User(id: Long = 0l,
-	username: String,
-	password: String,
-	passwordRepeat: Option[String] = Option(""),
-	email: String,
-	birth: Date) {
+                username: String,
+                password: String,
+                email: String,
+                birth: Date,
+                mobile: Option[String]) {
 
 }
