32bab03b7a17feec99591e2cc60d767e916c94ee
firefoxmmx2 <firefoxmmx2>
2013-08-26 23:21:09 +0800

    添加常量类

---
+++ src/com/example/infosearch/common/Constants.scala
@@ -0,0 +1,10 @@
+package com.example.infosearch.common
+
+object Constants {
+  val base_api_url = "http://localhost:9000"
+  val api_url_register = base_api_url + "/user/register"
+  val api_url_login = base_api_url + "/login"
+  val api_url_book_search = base_api_url + "/book/query"
+  val api_url_book_add = base_api_url + "/book/add"
+  val api_url_book_delete = base_api_url + "/book/delete"
+}
